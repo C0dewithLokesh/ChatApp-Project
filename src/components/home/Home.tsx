69902bbd106dc10ee693832ef7fc0ab337e39215
Lokesh <lokeshkaushik2243@gmail.com>
2023-07-19 09:09:16 +0530

    Home changes done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,12 @@
 import { IonPage } from "@ionic/react";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsSoundwave } from "react-icons/bs";
-import { BsFillFileEarmarkCodeFill } from "react-icons/bs";
 import userImg from "/user.jpg";
-import artLogo from "/code.png";
+import HomeCards from "./HomeCards";
 
 const Home = () => {
   return (
-    <IonPage className="p-2 justify-start gap-10 items-center overflow-scroll bg-white">
+    <IonPage className="home-page p-2 justify-start gap-10 items-center overflow-scroll">
       <div className="w-full flex justify-between items-center px-3 mt-5">
         <div className="user-img relative">
           <img src={userImg} alt="user-Img" className="w-11 rounded-[50%] overflow-hidden" />
@@ -18,50 +17,16 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="flex flex-col items-center rounded-[50%] gap-4 p-10 border-[2px] border-gray-400">
-        <p className="text-gray-500 font-medium">Hi, Todd 👋</p>
-        <h1 className=" text-2xl">Tap to Chat</h1>
-        <BsSoundwave size={'40px'} color="gray" />
+      <div className="bot-circle flex flex-col items-center rounded-[50%] gap-4 p-10 border-[0px] border-[#cdaae3] bg-[#e4cff1c3] relative">
+        <p className="text-gray-200 font-medium">Hi, Todd 👋</p>
+        <h1 className="bot-btn text-2xl text-black">Tap to Chat</h1>
+        <BsSoundwave size={'40px'} color="white" />
       </div>
 
       <div className="w-full flex flex-col gap-4">
-        <h1 className="p-3 text-3xl font-medium">Explore</h1>
+        <h1 className="p-3 text-3xl font-medium text-white">Explore</h1>
 
-        <div className="flex gap-3 flex-wrap justify-center">
-
-          <div className="p-2 px-3 w-[48%] flex flex-col bg-[#525151] rounded-[20px] text-white gap-3">
-            {/* <img src={artLogo} alt="art-logo" className=" w-10" /> */}
-            <BsFillFileEarmarkCodeFill color="lightgreen" size={'32px'} />
-            <div className="text">
-              <h1 className=" text-2xl font-medium">Code</h1>
-              <p>Write programming code or algorithms for complex applications.</p>
-            </div>
-          </div>
-
-          <div className="p-2 px-3 w-[48%] flex flex-col bg-[#525151] rounded-[20px] text-white gap-3">
-            <BsFillFileEarmarkCodeFill color="lightgreen" size={'32px'} />
-            <div className="text">
-              <h1 className=" text-2xl font-medium">Code</h1>
-              <p>Write programming code or algorithms for complex applications.</p>
-            </div>
-          </div>
-
-          <div className="p-2 px-3 w-[48%] flex flex-col bg-[#525151] rounded-[20px] text-white gap-3">
-            <BsFillFileEarmarkCodeFill color="lightgreen" size={'32px'} />
-            <div className="text">
-              <h1 className=" text-2xl font-medium">Code</h1>
-              <p>Write programming code or algorithms for complex applications.</p>
-            </div>
-          </div>
-          
-          <div className="p-2 px-3 w-[48%] flex flex-col bg-[#525151] rounded-[20px] text-white gap-3">
-            <BsFillFileEarmarkCodeFill color="lightgreen" size={'32px'} />
-            <div className="text">
-              <h1 className=" text-2xl font-medium">Code</h1>
-              <p>Write programming code or algorithms for complex applications.</p>
-            </div>
-          </div>
-        </div>
+        <HomeCards />
       </div>
     </IonPage>
   )
---
+++ src/components/home/HomeCards.tsx
@@ -0,0 +1,58 @@
+import { AiOutlineMessage } from "react-icons/ai";
+import { BsClipboard, BsFillFileEarmarkCodeFill } from "react-icons/bs";
+import { RiHandHeartFill } from "react-icons/ri";
+import { MdCardGiftcard } from "react-icons/md";
+import { FaFileInvoiceDollar } from "react-icons/fa";
+
+interface CardProps {
+  icon: JSX.Element;
+  title: string;
+}
+
+const Card: React.FC<CardProps> = ({ icon, title }) => {
+  return (
+    <div className="home-card p-2 px-3 w-[48%] flex flex-col bg-[#fff] rounded-[20px] text-black gap-3 items-center">
+      {icon}
+      <div className="text">
+        <h1 className="text-2xl font-medium text-center">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+const HomeCards = () => {
+
+  const cardData = [
+    {
+      icon: <BsClipboard size={"62px"} color="#8629bc"  />,
+      title: 'Survey',
+    },
+    {
+      icon: <AiOutlineMessage color="#8629bc" size={'62px'} />,
+      title: 'Leave',
+    },
+    {
+      icon: <RiHandHeartFill color="#8629bc" size={'62px'} />,
+      title: 'Core HR',
+    },
+    {
+      icon: <MdCardGiftcard color="#8629bc" size={'62px'} />,
+      title: 'Payroll',
+    },
+    {
+      icon: <FaFileInvoiceDollar color="#8629bc" size={'62px'} />,
+      title: 'Employee Benefits',
+    },
+    // Add more data objects for additional cards
+  ];
+
+  return (
+    <div className="flex gap-3 flex-wrap justify-start px-4">
+      {cardData.map((data, index) => (
+        <Card key={index} icon={data.icon} title={data.title} />
+      ))}
+    </div>
+  );
+};
+
+export default HomeCards;
